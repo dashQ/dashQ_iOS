eb0901e8a68ad0cabb18d1aa0709d2a7c50ec4c0
dashQ_Master <mydashq@gmail.com>
2016-01-01 23:49:43 +0900

    project created

---
+++ dashQ/dashQ/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  dashQ
+//
+//  Created by Hoyool Yoon on 1/1/16.
+//  Copyright (c) 2016 dashQ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ dashQ/dashQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dashQ
+//
+//  Created by Hoyool Yoon on 1/1/16.
+//  Copyright (c) 2016 dashQ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
